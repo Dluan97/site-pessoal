118f6f3481e9f8209f5a01194fcb88cc179d9cf3
Dluan97 <dluanlopes9@gmail.com>
2025-05-30 17:42:39 -0300

    Atualização de aparencia

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 
             .forEach(projeto => {
                 const div = document.createElement("div");
-             div.classList.add("projeto");
+             div.classList.add("projeto", "fade-in");
               div.innerHTML = `
                 <h3>${projeto.titulo}</h3>
                 <p>${projeto.descricao}</p>
@@ -17,6 +17,30 @@
             container.appendChild(div);
             } );
         }
+
+    // Validação e envio do formulário
+
+    const form = document.getElementById('form-contato');
+    const statusEnvio = document.getElementById('status-envio');
+
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // evita recarregar a página
+
+      const nome = document.getElementById('nome').value.trim();
+      const email = document.getElementById('email').value.trim();
+      const mensagem = document.getElementById('mensagem').value.trim();
+
+      if(nome === "" || email === "" || mensagem === "") {
+        statusEnvio.textContent = "Por favor, preencha todos os campos.";
+        statusEnvio.style.color = "red";
+        return;
+      }
+
+      statusEnvio.textContent = "Mensagem enviada com sucesso";
+      statusEnvio.style.color = "green";
+
+      form.reset();
+    });
   
     // Filtro ao digitar
 
